3a8490db2fb419cefc4efb92e951b8aac5e332c8
Leandro Luque <leandro.luque@gmail.com>
2017-06-08 14:26:29 -0300

    Some corrections to the class Circle were implemented.

--- webdiagrams/primitives.js
+++ webdiagrams/primitives.js
@@ -10,15 +10,12 @@
 
 class Primitive {
 
-    constructor(x = 0, y = 0, width = 50, height = 50, stylingAttributes = new StylingAttributes(), id) {
+    constructor(x = 0, y = 0, width = 50, height = 50, stylingAttributes = new StylingAttributes()) {
         this._x = x;
         this._y = y;
         this._width = width;
         this._height = height;
         this._stylingAttributes = stylingAttributes;
-        this._id = id;
-        this._drawed = null;
-        this._changerListener = null;
     }
 
     get x() {
@@ -27,7 +24,6 @@ class Primitive {
 
     set x(value) {
         this._x = value;
-        this.changerListener.changeX(this, value);
     }
 
     get y() {
@@ -36,7 +32,6 @@ class Primitive {
 
     set y(value) {
         this._y = value;
-        this.changerListener.changeY(this, value);
     }
 
     get width() {
@@ -45,7 +40,6 @@ class Primitive {
 
     set width(value) {
         this._width = value;
-        this.changerListener.changeWidth(this, value);
     }
 
     get height() {
@@ -54,7 +48,6 @@ class Primitive {
 
     set height(value) {
         this._height = value;
-        this.changerListener.changeHeight(this, value);
     }
 
     get stylingAttributes() {
@@ -63,38 +56,6 @@ class Primitive {
 
     set stylingAttributes(value) {
         this._stylingAttributes = value;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    set id(value) {
-        this._id = value;
-    }
-
-    get drawed() {
-        return this._drawed;
-    }
-
-    set drawed(value) {
-        this._drawed = value;
-    }
-
-    get changerListener() {
-        return this._changerListener;
-    }
-
-    set changerListener(value) {
-        this._changerListener = value;
-    }
-
-    move(newX, newY) {
-        this.changerListener.changePosition(this, newX, newY);
-    }
-
-    attr(json) {
-        this.changerListener.changeStylingAttributes(this, json);
     }
 
 }
@@ -119,12 +80,12 @@ class Circle extends Primitive {
     }
 
     set width(value) {
-        this._radius = value / 2;
+        this._radius = value/2;
         return super.width = value;
     }
 
     set height(value) {
-        this._radius = value / 2;
+        this._radius = value/2;
         return super.height = value;
     }
 
@@ -157,111 +118,9 @@ class Circle extends Primitive {
     }
 }
 
-class Ellipse extends Primitive {
-
-    constructor(centerX = 0, centerY = 0, radiusX = 50, radiusY = 25, stylingAttributes) {
-        super(centerX - radiusX, centerY - radiusY, radiusX * 2, radiusY * 2, stylingAttributes);
-        this._centerX = centerX;
-        this._centerY = centerY;
-        this._radiusX = radiusX;
-        this._radiusY = radiusY;
-    }
-
-    set x(value) {
-        this._centerX = value + this._radius;
-        return super.x = value;
-    }
-
-    set y(value) {
-        this._centerY = value + this._radius;
-        return super.y = value;
-    }
-
-    set width(value) {
-        this._radiusX = value / 2;
-        return super.width = value;
-    }
-
-    set height(value) {
-        this._radiusY = value / 2;
-        return super.height = value;
-    }
-
-    get centerX() {
-        return this._centerX;
-    }
-
-    set centerX(value) {
-        super.x = value - this._radiusX;
-        this._centerX = value;
-    }
-
-    get centerY() {
-        return this._centerY;
-    }
-
-    set centerY(value) {
-        super.y = value - this._radiusY;
-        this._centerY = value;
-    }
-
-    get radiusX() {
-        return this._radiusX;
-    }
-
-    set radiusX(value) {
-        this._radiusX = value;
-    }
-
-    get radiusY() {
-        return this._radiusY;
-    }
-
-    set radiusY(value) {
-        this._radiusY = value;
-    }
-
-}
-
-class Rectangle extends Primitive {
-
-    constructor(x1 = 10, y1 = 10, x2 = 100, y2 = 20, stylingAttributes) {
-        super(x1, y1, x2-x1, y2-y1, stylingAttributes);
-    }
-
-}
-
-class Text extends Primitive {
-
-    constructor(x = 10, y = 10, text = "Text", stylingAttributes) {
-        super(x, y, undefined, undefined, stylingAttributes);
-        this._text = text;
-    }
-
-    get width() {
-        let boundingBox = this.drawed.getBBox();
-        return boundingBox.width;
-    }
-
-    get height() {
-        let boundingBox = this.drawed.getBBox();
-        return boundingBox.height;
-    }
-
-    get text() {
-        return this._text;
-    }
-
-    set text(value) {
-        this._text = value;
-        this.changerListener.changeText(this, value);
-    }
-
-}
-
 class StylingAttributes {
 
-    constructor(strokeWidth = 1, strokeColor = 'black', fillColor = 'blue') {
+    constructor(strokeWidth = 1, strokeColor = 'black', fillColor = 'white') {
         this._strokeWidth = strokeWidth;
         this._strokeColor = strokeColor;
         this._fillColor = fillColor;
@@ -289,10 +148,6 @@ class StylingAttributes {
 
     set fillColor(value) {
         this._fillColor = value;
-    }
-
-    toString() {
-        return "stroke:" + this.strokeColor + "; fill: " + this.fillColor + "; stroke-width: " + this.strokeWidth;
     }
 
 }
